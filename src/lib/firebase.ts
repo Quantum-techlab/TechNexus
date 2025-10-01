623a5c24e0f0f5b500b1cabadf0e9f24b0ef1e08
Abdulrasaq Abdulrasaq Alatare <Ola283dayo@gmail.com>
2025-10-01 16:49:59 +0000

    Try fixing this error: `Console Error: [2025-10-01T16:49:31.237Z] @fireb

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,12 +4,12 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyBPFUj7VZv6ekJ4S5WyHv5U_trZcLLoc5g",
+  authDomain: "technexus-f1ac1.firebaseapp.com",
+  projectId: "technexus-f1ac1",
+  storageBucket: "technexus-f1ac1.firebasestorage.app",
+  messagingSenderId: "1006011805397",
+  appId: "1:1006011805397:web:6d62c902aef10d9ed87c5c",
 };
 
 function getFirebaseApp(): FirebaseApp {
